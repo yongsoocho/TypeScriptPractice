b5ceb45060145f38118645de4278a575185a3860
yongsoocho <yongsoocho578@gmail.com>
2021-04-06 23:54:07 +0000

    2021/04/07/1.0.2

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -5,6 +5,9 @@ function sum(a: number, b: number): number {
 // JS allow sum(10, 20, 30, 40..) But, TS is not
 // sum(10) will throw the error in TS
 
-function log(a: string, b: string) {
-	
-}
+function log(a: string, b?: string) {
+	return a + b;
+};
+
+// log('hi', 'world') is allowed
+// log('One') is allowd
