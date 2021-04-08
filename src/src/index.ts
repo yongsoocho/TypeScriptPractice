ce0f1ab532c7ecc00353814c3c3b090e558c34a8
yongsoocho <yongsoocho578@gmail.com>
2021-04-08 12:08:15 +0000

    BlockChain/2.0.0

--- src/src/index.ts
+++ src/src/index.ts
@@ -17,8 +17,23 @@ class Block {
 	static calculateBlockHash = (index: number, 
 								 previousHash: string, 
 								 timestamp: number, 
-								 data: string): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
-}
+								 data: string): string => {
+		return {
+			CryptoJS.SHA256(index + previousHash + timestamp + data).toString()
+		};
+	};
+
+	static validateStructure = (aBlock: Block): boolean => {
+		return (
+			typeof aBlock.index === 'number' &&
+			typeof aBlock.hash === 'string' &&
+			typeof aBlock.previousHash === 'string' &&
+			typeof aBlock.data === 'string' &&
+			typeof aBlock.timestamp === 'number'
+		);
+	};
+
+};
 
 const genesisBlock: Block = new Block(0, "0302", "", "HelloWorld", 20210408);
 
@@ -28,6 +43,32 @@ const getBlockChain = (): Block[] => blockchain;
 
 const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
-const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+	const previousBlock: Block = getLatestBlock();
+	const newIndex: number = previousBlock.index + 1;
+	const newTimestamp: number = getNewTimestamp();
+	const nextHash: string = Block.calculateBlockHash(newIndex,
+													 previousBlock.hash,
+													 newTimestamp,
+													 data
+													 );
+	const newBlock: Block = new Block(newIndex, nextHash,  previousBlock.hash, data, newTimestamp);
+	
+	return newBlock;
+}
+
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+	if(!validateStructure(candidateBlock)) {
+		return false
+	}else if(previousBlock.index + 1 !== candidateBlock.index) {
+		return false
+	}else if(previousBlock.hash !== candidateBlock.previousHash) {
+		return false
+	}
+};
 
 console.log(blockchain);
+console.log(createNewBlock("hello"));
+console.log(createNewBlock("byebye"));
